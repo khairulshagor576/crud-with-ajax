b5642f08ecf7160acc4edbaf833868c43a4e8c3b
Mohammad Khairul Islam <khairulsagor567@gmail.com>
2021-06-12 00:29:40 +0600

    a new project start name is: php crud with ajax

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+include "inc/header.php";
+?>
+
+<table border="1" cellspacing="1" cellpadding="1" width="100%">
+    <tr>
+       <th colspan="5"><button id="showData">Show Data</button></th>
+    </tr>
+    <tr>
+        <th>SL</th>
+        <th>Name</th>
+        <th>Email</th>
+        <th>Skill</th>
+        <th>Action</th>
+    </tr>
+</table>
+
+
+
+<?php include "inc/footer.php"?>
